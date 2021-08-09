c499783f1cad1a2e213a2fdcfca7961b41450594
aw <greydelta77@gmail.com>
2021-08-09 22:00:00 +0800

    added controller class

---
+++ src/Assignment/Controller.java
@@ -0,0 +1,53 @@
+package Assignment;
+import java.util.*;
+
+import Assignment.Package;
+
+public class Controller {
+
+	// ----------------------------------- [ VARIABLES ]
+	private DataListInterface DataLists;
+	
+	// ----------------------------------- [ CONSTRUCTOR ]
+	public Controller(DataListInterface DataLists) {
+		this.DataLists = DataLists;
+	}
+
+	// ----------------------------------- [ ACCESSOR METHODS ]
+	public ArrayList<Package> getAllPackages() {
+		return DataLists.getAllPackages();
+	}
+	
+	public ArrayList<Truck> getAllTrucks() {
+		return DataLists.getAllTrucks();
+	}
+	
+	// ----------------------------------- [ METHODS ]
+	public void add(Package tempPackage) {
+		DataLists.add(tempPackage);
+	}
+	
+	public void add(Truck tempTruck) {
+		DataLists.add(tempTruck);
+	}
+	
+	public void clearAllPackages() {
+		DataLists.clearAllPackages();
+	}
+	
+	public void clearAllTrucks() {
+		DataLists.clearAllTrucks();
+	}
+	
+	public int getTotalNumOfPackages() {
+		return DataLists.getTotalNumOfPackages();
+	}
+	
+	public int getTotalNumOfTrucks() {
+		return DataLists.getTotalNumOfTrucks();
+	}
+	
+	public void loadTruck(int truckID, Package pack, int capacity) {
+		DataLists.loadTruck(truckID, pack, capacity);
+	}
+}
